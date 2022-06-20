7124d07b00e376016a7f9d045c44519d43dee2ad
Duex_ <long.shangqing@gempoll.com>
2022-06-20 12:40:59 +0800

    number?????

--- questions/11-easy-tuple-to-object/template.ts
+++ questions/11-easy-tuple-to-object/template.ts
@@ -1,1 +1,7 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {
+  [P in  T[number]] :  P ;
+}
+
+const test = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type resultTest = TupleToObject<typeof test>
