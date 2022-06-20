fa79ceab7cc84f1bd6b9131ef0dd800f7a25ac89
Duex_ <long.shangqing@gempoll.com>
2022-06-20 17:39:07 +0800

    await

--- questions/189-easy-awaited/template.ts
+++ questions/189-easy-awaited/template.ts
@@ -1,1 +1,3 @@
-type MyAwaited = any
+type MyAwaited<T> = T extends Promise<infer E> ? E extends Promise<any> ? MyAwaited<E> : E : any
+
+// T 约束于 Promise<infer E> 就返回E
--- questions/43-easy-exclude/template.ts
+++ questions/43-easy-exclude/template.ts
@@ -1,1 +1,3 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> =  T extends U ? never : T
+
+// T 和 U是独立的类型 有交集 但不全一样 
